c6874d02d142d4a4d0b84e053428ceca9354f4dd
backend-kms <mindu6424@naver.com>
2026-03-11 16:15:31 +0900

    test: post json 반환

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ * createdAt: Date;
+ * updatedAt: Date;
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  getPost(): Post {
+    return {
+      author: 'John',
+      title: 'Hello World',
+      content: 'This is my first post',
+      likeCount: 10,
+      commentCount: 5,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
   }
 }
