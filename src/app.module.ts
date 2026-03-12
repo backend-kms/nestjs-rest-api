0c922f514fcfd6e675bbeea9d38d0eac4f643569
backend-kms <mindu6424@naver.com>
2026-03-12 11:04:47 +0900

    feat: PostsModel 엔티티 추가 및 TypeORM 설정 업데이트

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModel } from './posts/entities/posts.entity';
 
 @Module({
   // imports: 다른 모듈을 불러와서 사용할 수 있게 하는 역할
@@ -15,7 +16,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'postgres',
       password: 'postgres',
       database: 'postgres',
-      entities: [],
+      entities: [
+        PostsModel,
+      ],
       synchronize: true, // typeorm이 애플리케이션 실행 시 데이터베이스와 엔티티를 동기화하는 옵션, 개발 환경에서만 true로 설정하는 것이 좋음
     }),
   ],
