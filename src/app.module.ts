5175cfb0c18a78d1800aeea76958a3401062a19c
backend-kms <mindu6424@naver.com>
2026-03-12 10:07:16 +0900

    docs: 주석 추가로 코드 이해도 향상

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
+  // imports: 다른 모듈을 불러와서 사용할 수 있게 하는 역할
   imports: [PostsModule],
   controllers: [AppController],
   providers: [AppService],
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,6 +3,8 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 
 @Module({
+  // controllers: 클라이언트의 요청을 처리하는 역할
+  // providers: 서비스와 상호작용하여 비즈니스 로직을 처리하는 역할
   controllers: [PostsController],
   providers: [PostsService],
 })
